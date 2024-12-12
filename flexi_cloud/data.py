a9762ef8542e31c53a27bd5a7d8a8c6aad50ffb7
Narongdech <158162928+NarongdechP@users.noreply.github.com>
2024-12-12 14:02:20 +0700

    Up load source code

---
+++ flexi_cloud/data.py
@@ -0,0 +1,11 @@
+why = {
+	1: ["Smart Storage with Advanced Compression","Maximize your storage efficiency! Flexi Cloud uses cutting-edge compression technology to shrink your file sizes, enabling you to store more data without compromising quality. Enjoy reduced storage costs and a more organized cloud environment."],
+
+	2: ["Rapid File Access","Get your files in a flash. Our sophisticated decompression technology ensures that your data is quickly and efficiently delivered to you. What’s compressed in the cloud is effortlessly decompressed on our servers, so you can access your files instantly."],
+
+	3: ["Top-Notch Security","Protecting your data is our priority. With Amazon S3’s secure infrastructure combined with our advanced encryption methods, Flexi Cloud keeps your files safe from unauthorized access while maintaining their integrity."],
+
+	4: ["Scalable and Reliable","From small projects to large-scale enterprises, Flexi Cloud scales to meet your needs. Count on us for reliable performance and consistent service, no matter the size of your data."],
+	
+	5: ["Seamless Integration","Flexi Cloud integrates effortlessly with your existing systems and applications. Our user-friendly API and comprehensive support documentation make setup and integration smooth and straightforward."],
+}
---
+++ flexi_cloud/static/header.js
@@ -0,0 +1,22 @@
+content = `
+        <header>
+    		<nav>
+                <figure>
+                    <a href="/" id="header">
+                        <img src="./static/logo.gif" alt="Logo gif Goes here!">
+                        <div>
+                            <h1>Flexi Cloud</h1>
+                            <em>Empower Your Data with Flexi Cloud</em>
+                        </div>
+                    </a>
+                </figure>
+                <div class="nav-links">
+                    <a href="/#about" class="btns">About Us</a>
+                    <a href="/signin" class="btns">Sign-in</a>
+                    <a href="/signup" class="btns">Sign-up</a>
+                </div>
+            </nav>
+        </header>
+`
+
+document.write(content)
