26557cdc467e0830abef76dadc3dc2e3302923f8
jiraporn-dada <jiraporn.boons@dome.tu.ac.th>
2024-12-14 02:53:06 +0700

    move

--- DTI231_Project/apps.py
+++ DTI231_Project/apps.py
@@ -1,5 +1,28 @@
-from django.apps import AppConfig
+from flask import Flask, render_template
+app = Flask(__name__)
 
-class YourAppNameConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'NachDaTee'
+@app.route('/')
+def all_file():
+    return render_template("All file.html")
+
+@app.route('/home')
+def home():
+    return render_template("Home.html")
+
+@app.route('/profile')
+def profile():
+    user = {"Username":"dada","Email":"jiraporn.boonsob@gmail.com"}
+    return render_template("Profile.html",username= user)
+
+@app.route('/sign_in')
+def sign_in():
+    return render_template("Sign in.html")
+
+@app.route('/sign_up')
+def sign_up():
+    return render_template("Sign up.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
