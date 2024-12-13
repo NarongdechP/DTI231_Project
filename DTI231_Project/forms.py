9fb34bab586ea146bb5dbb59a595a5096d1ab516
jiraporn-dada <jiraporn.boons@dome.tu.ac.th>
2024-12-14 03:19:09 +0700

    เคลียร์ฟีเจอร์ที่ไม่ได้ใช้

--- DTI231_Project/forms.py
+++ DTI231_Project/forms.py
@@ -30,33 +30,3 @@ class UserRegistration(UserCreationForm):
         raise forms.ValidationError(f"The {user.username} mail is already exists/taken")
 
 
-class UpdateProfile(UserChangeForm):
-    username = forms.CharField(max_length=250,help_text="The Username field is required.")
-    email = forms.EmailField(max_length=250,help_text="The Email field is required.")
-    current_password = forms.CharField(max_length=250)
-
-    class Meta:
-        model = User
-        fields = ('email', 'username','first_name', 'last_name')
-
-    def clean_current_password(self):
-        if not self.instance.check_password(self.cleaned_data['current_password']):
-            raise forms.ValidationError(f"Password is Incorrect")
-
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        try:
-            user = User.objects.exclude(id=self.cleaned_data['id']).get(email = email)
-        except Exception as e:
-            return email
-        raise forms.ValidationError(f"The {user.email} mail is already exists/taken")
-
-    def clean_username(self):
-        username = self.cleaned_data['username']
-        try:
-            user = User.objects.exclude(id=self.cleaned_data['id']).get(username = username)
-        except Exception as e:
-            return username
-        raise forms.ValidationError(f"The {user.username} mail is already exists/taken")
-
-
